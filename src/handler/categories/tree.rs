5f0014aa0d5a72f0a1a89350fbba7043c949d2d1
Lucas chen <lucas.chen@gbo-asia.com>
2024-09-12 17:45:31 +0800

    :tada: completed categories tree

--- src/handler/categories/tree.rs
+++ src/handler/categories/tree.rs
@@ -1,8 +1,11 @@
+use std::collections::VecDeque;
+
 use crate::app_state::AppState;
 use axum::{
-    extract::{Query, State},
-    response::IntoResponse,
+    extract::{Query, State}, http::StatusCode, response::IntoResponse, Json
 };
+
+use super::list::Category;
 
 #[derive(Debug, serde::Deserialize)]
 pub struct TreeParams {
@@ -34,9 +37,27 @@ pub async fn handler(
         .await
         .unwrap();
 
-    // TODO tree()
+    let tree_data = build_tree(&rows, 0);
+
+    (StatusCode::OK, Json(serde_json::json!({
+        "message": "ok",
+        "data": tree_data
+    })))
 }
 
-fn build_tree(categories: Vec<CategoryRow>) {
-    // TODO
+fn build_tree(categories: &Vec<CategoryRow>, parent_id: u64) -> Vec<TreeNode> {
+    let mut result = Vec::<TreeNode>::with_capacity(8);
+
+    categories.iter().for_each(|category| {
+        if category.parent_id == parent_id {
+            let children: Vec<TreeNode> = build_tree(categories, category.id);
+            result.push(TreeNode {
+                id: category.id,
+                name: category.name.clone(),
+                children: children,
+            })
+        }
+    });
+
+    return result;
 }
